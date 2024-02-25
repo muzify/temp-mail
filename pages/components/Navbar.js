ba7e7db3661b49b6b20a6f41efd5a620d7581c20
fortnitelookup <150611594+muzify@users.noreply.github.com>
2024-02-26 00:32:30 +0100

    Update Navbar.js

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from "next/link";
 import { MdOutlineAlternateEmail } from "react-icons/md"
 import { BsGithub , BsLinkedin } from "react-icons/bs"
-import { TbBrandFortnite } from "react-icons/tb"
 
 const Navbar = () => {
   return (
@@ -11,7 +10,6 @@ const Navbar = () => {
       <div className="p-3 container d-flex justify-content-between align-items-center">
       <p className="mt-2 h4 text-center font-boo temp-mail text-white" style={{cursor:"pointer"}}><MdOutlineAlternateEmail className="mail-logo text-danger"/>TEMP<span className="text-danger border border-2 border-danger">MAIL</span></p>
     <div>
-    <Link href="https://fortnitefinder.vercel.app"><a className="link-light" target="_blank"><TbBrandFortnite className="fs-4 github-icon"/></a></Link>
     </div>
     </div>
     </nav>
